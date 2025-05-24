a79275a9fe60d3530bd531af1575a34023ba64e2
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-24 12:23:43 +0200

    adding to the repo a new tappy game

---
+++ Tappy/scenes/plane/Plane.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const JUMP_POWER: float = -350.0
+
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(_gravity)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+func _physics_process(delta: float) -> void:
+	velocity.y += _gravity * delta
+	
+	
+	if Input.is_action_just_pressed("jump") == true:
+		velocity.y = JUMP_POWER
+	
+	move_and_slide()
