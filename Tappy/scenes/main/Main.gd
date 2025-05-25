f22634806a20842f059222a3ff9475196e735ca7
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-25 13:08:16 +0200

    adding music to the main scene, and changing to the game scene when a player hits the space bar

---
+++ Tappy/scenes/main/Main.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+const GAME = preload("res://scenes/game/Game.tscn")
+
+
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("jump") == true:
+		#print("_input:", event)
+		#get_viewport().set_input_as_handled()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") == true:
+		print("_unhandled_input:", event)
+		get_tree().change_scene_to_packed(GAME)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
