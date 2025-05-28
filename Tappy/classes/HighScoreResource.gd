0badd01586eb5d3d55c63e74939fbfe45c56bf0c
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-28 18:55:10 +0200

    adding to the project a custom resource for saving the high score on the users route

---
+++ Tappy/classes/HighScoreResource.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+
+class_name HighScoreResource
+
+
+@export var high_score: int = 0
+
+
+#%APPDATA%\Godot\app_userdata\[project_name]
+#C:\Users\joseo\AppData\Roaming\Godot\app_userdata\Tappy
