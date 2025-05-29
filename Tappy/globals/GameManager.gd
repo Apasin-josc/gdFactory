4ea666c4f9561fbcfc512ebd83c0d195958e424b
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-29 19:19:44 +0200

    adding a simple transition to the game

--- Tappy/globals/GameManager.gd
+++ Tappy/globals/GameManager.gd
@@ -2,11 +2,14 @@ extends Node
 
 const GAME = preload("res://scenes/game/Game.tscn")
 const MAIN = preload("res://scenes/main/Main.tscn")
+const SIMPLE_TRANSITION = preload("res://scenes/simpletransition/SimpleTransition.tscn")
 
-
+var next_scene: PackedScene
 
 func load_game_scene() -> void:
-	get_tree().change_scene_to_packed(GAME)
+	next_scene = GAME
+	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
 
 func load_main_scene() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	next_scene = MAIN
+	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
