e079ad3c5ee850cd03e1c3963561ab471fe1ec17
omarscoppola <joseomar_coppola@hotmail.com>
2025-09-21 13:18:38 +0200

    fixing the game, 2d platformer checked

--- 2dPlatformer/Scripts/end_flag.gd
+++ 2dPlatformer/Scripts/end_flag.gd
@@ -5,5 +5,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
 		return
-		
+	
+	call_deferred("level_passed")
+
+func level_passed() -> void:
 	get_tree().change_scene_to_packed(scene_to_load)
